180681e01225f43c18ba243b383fb15a6babcac9
Kristina Koshel <koshel.ko@dns-shop.ru>
2025-10-15 09:53:35 +1000

    feat: add authorization with auth jwt

--- src/app/api/v1/routers.py
+++ src/app/api/v1/routers.py
@@ -1,8 +1,20 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException, Response
 
-router = APIRouter()
+from app.config import config, security
+from app.schema.auth import UserLoginSchema
+
+router = APIRouter(tags=["Authorization"])
 
 
-@router.get("/health")
-async def check_health() -> dict[str, str]:
-    return {"status": "ok"}
+@router.post("/login")
+def login(creds: UserLoginSchema, response: Response):
+    if creds.username == "uname" and creds.password == "test":
+        token = security.create_access_token(uid="123")
+        response.set_cookie(config.JWT_ACCESS_COOKIE_NAME, token)
+        return {"access_token": token}
+    raise HTTPException(status_code=401, detail="Incorrect username or password")
+
+
+@router.get("/protected", dependencies=[Depends(security.access_token_required)])
+def protected():
+    return {"data": "Protected data"}
--- src/app/main.py
+++ src/app/main.py
@@ -1,6 +1,9 @@
-from fastapi import FastAPI
+from fastapi import APIRouter, FastAPI
 
-from app.api.v1.routers import router
+from app.api.v1.routers import router as v1_router
 
 app = FastAPI()
+
+router = APIRouter(prefix="/api")
+router.include_router(v1_router)
 app.include_router(router=router)
